aa44b35f1a7f11a81d77a44f5f580c3a8ad10473
Abhishek Kumar <abhishekkumar887353@gmail.com>
2024-03-11 20:58:51 +0530

    newmsg

--- models/listing.js
+++ models/listing.js
@@ -41,15 +41,15 @@ const sampleListing = new Schema({
     },
 
     geometry: {
-        type: {
-            type: String, // Don't do `{ location: { type: String } }`
-            enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
-          },
-          coordinates: {
-            type: [Number],
-            required: true
-          },
+          type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
     }
 
 });
